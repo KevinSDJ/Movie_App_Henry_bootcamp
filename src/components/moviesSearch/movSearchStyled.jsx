deca275c43676eb9b42d68bc8e43c70ac8ed314d
KevinSDJ <sebadjkevin@gmail.com>
2021-12-21 17:42:57 -0300

    .

--- src/components/moviesSearch/movSearchStyled.jsx
+++ src/components/moviesSearch/movSearchStyled.jsx
@@ -28,7 +28,7 @@ export const SearchsCont= styled.div`
 export const Card= styled.div`
    position:relative;
    list-style: none;
-   min-width:200px ;
+   width:200px ;
    height: 300px;
    img{
        position:absolute;
